26c1c72b30ae70625b3a2a690278281623077f35
Amanda Borges <107406443+AmandaMacBorges@users.noreply.github.com>
2023-01-25 14:54:55 -0300

    Add files via upload

--- multiplicadorDoisOperandos/multiplicadorDoisOperandos.asm
+++ multiplicadorDoisOperandos/multiplicadorDoisOperandos.asm
@@ -1,42 +1,42 @@
 .code
-              lda    a
-              jz     FIM
-              lda    b
-              jz     FIM
+              lda    a        ;carrega a em AC
+              jz     FIM      ;se for zero encerra programa
+              lda    b        ;carrega b em AC
+              jz     FIM      ;se for zero encerra programa
 
-INICIO:       lda    b
-              not
-              add    #01h
-              add    a
-              jn     menorA
+INICIO:       lda    b        ;recarrega b em AC
+              not             ;faz compl. C-1 em b
+              add    #01h     ;faz compl. C-2 em b
+              add    a        ;realiza operacao (a-b)
+              jn     menorA   ;se (a-b)<0 entao (a<b)
 
-menorB:       lda    b
-              add    #0ffh
-              sta    b
+menorB:       lda    b        ;caso (b<a) carrega b em AC
+              add    #0ffh    ;decrementa b em 1 
+              sta    b        ;armazena resultado em b
 
-              jn     FIM
+              jn     FIM      ;se (b-1)<0 encerra programa
 
-              lda    resultado
-              add    a
-              sta    resultado
-              jmp    menorB
+              lda    resultado    ;carrega resultado em AC
+              add    a            ;realiza (resultado+a)
+              sta    resultado    ;armazena soma em resultado
+              jmp    menorB       ;reinicia todo processo
 
-menorA:       lda    a
-              add    #0ffh
-              sta    a
+menorA:       lda    a            ;caso (a<b) carrega a em AC
+              add    #0ffh        ;decrementa a em 1
+              sta    a            ;armazena resultado em a
 
-              jn     FIM
+              jn     FIM          ;se (a-1)<0 encerra programa
 
-              lda    resultado
-              add    b
-              sta    resultado
-              jmp    menorA
+              lda    resultado    ;carrega resultado em AC
+              add    b            ;realiza (resultado+b)
+              sta    resultado    ;armazena soma em resultado
+              jmp    menorA       ;reinicia todo processo
 
-FIM:          hlt
+FIM:          hlt                 ;por fim, encerra programa
 .endcode
 
 .data
-a:            db      #04h
-b:            db      #02h
-resultado:    db      #00h
+a:            db      #04h        ;valor arbitrado para a
+b:            db      #02h        ;valor arbitrado para b
+resultado:    db      #00h        ;var. q armazena resultado final
 .enddata
