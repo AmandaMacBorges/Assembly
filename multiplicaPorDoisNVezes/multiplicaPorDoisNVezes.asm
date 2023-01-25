35a7247a23d2e7eb97d8cca0ead25591620776c6
Amanda Borges <107406443+AmandaMacBorges@users.noreply.github.com>
2023-01-25 14:44:17 -0300

    Add files via upload

--- multiplicaPorDoisNVezes/multiplicaPorDoisNVezes.asm
+++ multiplicaPorDoisNVezes/multiplicaPorDoisNVezes.asm
@@ -1,3 +1,5 @@
+;Requisito do problema: range do loop eh (i=0) ate (i<N)
+
 .code
 INICIO:    lda    N           ;carrega o valor b em AC
            not                ;faz o compl. C-1 de b
@@ -7,19 +9,13 @@ INICIO:    lda    N           ;carrega o valor b em AC
            jz     FIM         ;se (i-N)=0 encerra programa
 
 OPERACAO:  lda    i           ;carrega i atual em AC
-           sta    auxMult     ;guarda valor de i em auxMult
+           add    i
+           add    X           ;realiza (i+i+X) em AC
+           sta    X           ;armazena resultado em X
 
-           lda    X           ;carrega X atual em AC
-           sta    auxSum      ;guarda valor de X em auxSum
-
-           lda    auxMult     ;carrega aux em AC
-           add    i           ;soma valor armazenado em AC com i
-           add    auxSum      ;soma valor armazenado em AC com auxSum
-           sta    X           ;armazena valor de AC (duas somas) em X
-
-           lda    i           ;carrega i em AC
-           add    #01h        ;incrementa valor armazenado em AC em 1
-           sta    i           ;armazena valor calculado em AC em i
+           lda    i           ;recarrega i em AC
+           add    #01h        ;incrementa em 1
+           sta    i           ;armazena valor incrementado 
            jmp    INICIO      ;repete todo processo
 
 FIM:       hlt
