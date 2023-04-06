9605832ae100db754d69845ed3d91721e1165400
Amanda Borges <107406443+AmandaMacBorges@users.noreply.github.com>
2023-04-06 11:36:30 -0300

    Update LP1_ex2.asm

--- LP1_ex2.asm
+++ LP1_ex2.asm
@@ -1,20 +1,76 @@
 ; dado um inteiro positivo n, verificar se n eh primo
 
-%INCLUDE "io.inc"
+section .data
+    msg db 'O numero eh primo', 0
+    not_prime db 'O numero nao eh primo', 0
 
-SECTION .data
+section .bss
+    n resb 4
 
-num DD 07
+section .text
+    global _start
 
-SECTION .text
-global MAIN
+_is_prime:
+    mov eax, dword [esp+4]
+    cmp eax, 2
+    jle _not_prime
 
-MAIN:
-    ;aqui vamos armazenar os nros primos num vetor
+    mov ecx, 2
+_loop:
+    mov edx, 0
+    div ecx
+    cmp edx, 0
+    je _not_prime
+    inc ecx
+    cmp ecx, eax
+    jle _loop
 
+    ret
 
-    ;aqui vamos ir fazendo as divisoes
-    MOV EDX, 0        ; EDX contera o resto
-    MOV EAX, [num]    ; EAX o numerador
-    MOV ECX, 
-    
+_start:
+    ; le o numero inteiro do usuario
+    mov eax, 3
+    mov ebx, 0
+    mov ecx, n
+    mov edx, 4
+    int 0x80
+
+    ; converte a entrada do usuario em um numero inteiro
+    mov eax, dword [n]
+    sub eax, 0x30
+
+    ; chama a funcao _is_prime para verificar se o numero eh primo
+    push eax
+    call _is_prime
+    add esp, 4
+
+    ; exibe a mensagem correspondente
+    cmp eax, 0
+    jne _not_prime_msg
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, msg
+    mov edx, 16
+    int 0x80
+    jmp _exit
+_not_prime_msg:
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, not_prime
+    mov edx, 21
+    int 0x80
+_exit:
+    ; finaliza o programa
+    mov eax, 1
+    xor ebx, ebx
+    int 0x80
+
+_not_prime:
+    xor eax, eax
+    ret
+
+;Explicação do código:
+
+    ;A seção .data contém as mensagens que serão exibidas ao usuário para informar se o número é primo ou não.
+
+    ;A seção .bss reserva espaço na memória para a variável n, que armazenará o número inteiro
