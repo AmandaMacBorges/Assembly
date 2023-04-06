9070a4ebc9cd273be5bbf2289b24cb5afc6ff72c
Amanda Borges <107406443+AmandaMacBorges@users.noreply.github.com>
2023-04-06 11:33:28 -0300

    Create LP1_ex2.asm

---
+++ LP1_ex2.asm
@@ -0,0 +1,20 @@
+; dado um inteiro positivo n, verificar se n eh primo
+
+%INCLUDE "io.inc"
+
+SECTION .data
+
+num DD 07
+
+SECTION .text
+global MAIN
+
+MAIN:
+    ;aqui vamos armazenar os nros primos num vetor
+
+
+    ;aqui vamos ir fazendo as divisoes
+    MOV EDX, 0        ; EDX contera o resto
+    MOV EAX, [num]    ; EAX o numerador
+    MOV ECX, 
+    
