e1c6d247283ae72e96263ba8f89b1b92f2b02d6d
Amanda Borges <107406443+AmandaMacBorges@users.noreply.github.com>
2023-04-06 15:46:19 -0300

    Update LP1_ex2.asm

--- LP1_ex2.asm
+++ LP1_ex2.asm
@@ -1,76 +1,39 @@
-; dado um inteiro positivo n, verificar se n eh primo
-
 section .data
-    msg db 'O numero eh primo', 0
-    not_prime db 'O numero nao eh primo', 0
-
-section .bss
-    n resb 4
+    n dd 29        ; numero escolhido
+    primo dd 0     ; flag primo
+    nn_p dd 0      ; flag nn primo
 
 section .text
     global _start
 
-_is_prime:
-    mov eax, dword [esp+4]
-    cmp eax, 2
-    jle _not_prime
-
-    mov ecx, 2
-_loop:
-    mov edx, 0
-    div ecx
-    cmp edx, 0
-    je _not_prime
-    inc ecx
-    cmp ecx, eax
-    jle _loop
-
-    ret
-
 _start:
-    ; le o numero inteiro do usuario
-    mov eax, 3
-    mov ebx, 0
-    mov ecx, n
-    mov edx, 4
-    int 0x80
+    mov eax, [n]    ; carrega o valor de n em eax
+    cmp eax, 2      ; compara com 2
+    jl na_prim      ; se n < 2, nao eh primo
 
-    ; converte a entrada do usuario em um numero inteiro
-    mov eax, dword [n]
-    sub eax, 0x30
+    mov ecx, 2      ; inicializa o divisor em 2, primeiro primo
+    
+verifica_div:
+    cmp ecx, eax    ; verifica se o divisor eh maior ou igual ao valor de n
+    jge eh_prim     ; se sim, n eh primo
 
-    ; chama a funcao _is_prime para verificar se o numero eh primo
-    push eax
-    call _is_prime
-    add esp, 4
+    mov edx, 0      ; limpa edx para armazenar o resto da divisao
+    div ecx         ; divide eax por ecx, armazenando o quociente em eax e o resto em edx
+    cmp edx, 0      ; verifica se o resto é zero
+    je na_prim
+    inc ecx         ; incrementa o divisor
+    jmp verifica_div
 
-    ; exibe a mensagem correspondente
-    cmp eax, 0
-    jne _not_prime_msg
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, msg
-    mov edx, 16
-    int 0x80
-    jmp _exit
-_not_prime_msg:
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, not_prime
-    mov edx, 21
-    int 0x80
-_exit:
-    ; finaliza o programa
-    mov eax, 1
-    xor ebx, ebx
-    int 0x80
+eh_prim:
+    mov eax, [primo]
+    add eax, 1
+    mov [primo], eax
+    jmp end
 
-_not_prime:
-    xor eax, eax
-    ret
+na_prim:
+    mov eax, [nn_p]
+    add eax, 1
+    mov [nn_p], eax
 
-;Explicação do código:
-
-    ;A seção .data contém as mensagens que serão exibidas ao usuário para informar se o número é primo ou não.
-
-    ;A seção .bss reserva espaço na memória para a variável n, que armazenará o número inteiro
+end: 
+    int 0x80        ; termina o programa
